cfcb182fc7f89d42f917c323317b8afe34a65639
BuildTools <unconfigured@null.spigotmc.org>
2021-02-08 21:12:49 -0600

    Adding flightcontroller.py
    The initial barebones version of the file with only a few comments and a print statement

---
+++ flightcontroller.py
@@ -0,0 +1,4 @@
+# This is the main script file that will regulate the raspberry pi's communication with input and output to flight controllers. Feel free to edit this code and push to main branch.
+# For big changes to the code, push to master branch and generate a pull request into the main branch. This way, code can be easily controlled with caution.
+
+print("Welcome to the SLES Robotics Drone Project. The Flight Controller Script is underway. \nHave fun you rascals.")
