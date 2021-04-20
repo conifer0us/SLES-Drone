e52acf88e468c4ac097816e6a7e247c93ed03b84
BuildTools <unconfigured@null.spigotmc.org>
2021-04-20 15:14:43 -0500

    Minor changes to listen_stop.py
    Made changes to ssh connection on closing script

--- listen_stop.py
+++ listen_stop.py
@@ -1,5 +1,6 @@
 import os
 
-pids = os.popen("fuser 9000/tcp").read().replace("  "," ").split(" ").remove('')
+pids = os.popen("fuser 9000/tcp").read().replace("  "," ").split(" ")
+pids.remove(" ")
 for i in pids:
     os.popen('kill ' + i)
