5a601c568dc2b3c19dfe73188e31fae9cae38a38
BuildTools <unconfigured@null.spigotmc.org>
2021-04-23 15:05:25 -0500

    Fixes bug in listen_stop

--- listen_stop.py
+++ listen_stop.py
@@ -9,7 +9,7 @@ def killall():
 os.popen("echo listen_stop activated and will shut program down when p is pressed. | nc 172.20.10.9 9001")
 
 while True:
-    shutdown = input()
+    shutdown = input().replace("\n", "")[0]
     if shutdown == "p":
         killall()
         exit()
