6bbb519a57673ef7ce018635bc461efef42347c5
BuildTools <unconfigured@null.spigotmc.org>
2021-04-26 15:20:42 -0500

    changes stuff for listen_stop

--- listen_stop.py
+++ listen_stop.py
@@ -1,16 +1,16 @@
 import os
 
 def killall():    
-    pids = os.popen("fuser 9000/tcp").read().replace("  "," ").split(" ")
+    pids = os.popen("sudo fuser 9000/tcp").read().replace("  "," ").split(" ")
     pids.remove("")
-    for i in pids:
-        os.popen('kill ' + i)
+    for pid in pids:
+        os.popen('sudo kill ' + pid)
 
 os.popen("echo \nListen_stop activated and will shut program down when p is pressed. | nc 172.20.10.9 9001")
 
 while True:
-    shutdown = input().replace("\n", "")[0]
-    if shutdown == "p":
+    shutdown = input()[0]
+    if shutdown == 'p':
         killall()
         os.popen("echo \n\n \*\*\*Shutdown Signal Received. Program is shutting down\*\*\* | nc 172.20.10.9 9001")
         exit()
