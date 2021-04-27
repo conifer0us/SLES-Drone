827950adbb62633fa293e980b651566217aec3ac
BuildTools <unconfigured@null.spigotmc.org>
2021-04-27 14:56:05 -0500

    Removed newlines from listen_stop

--- listen_stop.py
+++ listen_stop.py
@@ -6,8 +6,8 @@ def killall():
     for pid in pids:
         os.popen('sudo kill ' + pid)
 
-os.popen("echo \nListen_stop activated and will shut program down when p is pressed. | nc 172.20.10.9 9001")
-input()
+os.popen("echo Listen_stop activated and will shut program down when p is pressed. | nc 172.20.10.9 9001")
+input() # Stop until kill signal
 killall()
-os.popen("echo \n\n \*\*\*Shutdown Signal Received. Program is shutting down\*\*\* | nc 172.20.10.9 9001")
+os.popen("echo ***Shutdown Signal Received. Program is shutting down*** | nc 172.20.10.9 9001")
 exit()
