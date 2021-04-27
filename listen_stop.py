54f945b28929fec3a4b661ce894fa0c2bf00e24e
slesrobotics <slesrobotics@stlukesmobile.com>
2021-04-27 15:46:56 -0500

    SIGKILL added to listen_stop

--- listen_stop.py
+++ listen_stop.py
@@ -5,5 +5,5 @@ input() # Stop until kill signal
 os.popen("echo ***Shutdown Signal Received. Program is shutting down*** | nc 172.20.10.9 9001")
 pids = os.popen("sudo fuser 9000/tcp").read().replace("  "," ").split(" ")
 for pid in pids:
-    os.popen('sudo kill ' + pid)
+    os.popen('sudo kill -SIGKILL ' + pid)
 exit()
