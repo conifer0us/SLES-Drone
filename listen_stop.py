72f986af0b775d4362dacfd04ad3ee8d0fb78b64
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 17:10:36 -0500

    Fixes error in listen_stop

--- listen_stop.py
+++ listen_stop.py
@@ -3,6 +3,6 @@ import os
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
 pids = os.system("fuser 9000/tcp").read().split()
-print("Killing " + " ".split(pids))
-os.popen('sudo kill ' + " ".split(pids))
+print("Killing " + " ".join(pids))
+os.popen('sudo kill ' + " ".join(pids))
 exit()
