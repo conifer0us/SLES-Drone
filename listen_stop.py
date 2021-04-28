e6d49929af12cdab1e93cb461699c98b36b8d2cf
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 17:08:10 -0500

    Changes to os.system instead of popen to allow program to fully read files before moving on to other code
    system instead of popen will prevent listen_stop from exiting due to errors

--- listen_stop.py
+++ listen_stop.py
@@ -2,7 +2,7 @@ import os
 
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
-pids = os.popen("fuser 9000/tcp").read().split()
+pids = os.system("fuser 9000/tcp").read().split()
 print("Killing " + " ".split(pids))
 os.popen('sudo kill ' + " ".split(pids))
 exit()
