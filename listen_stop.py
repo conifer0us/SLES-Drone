4daef13bf229b4f00748edc322053e78cb62ecff
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 17:19:47 -0500

    Uses -k option for fuser in listen_stop to condense

--- listen_stop.py
+++ listen_stop.py
@@ -1,12 +1,6 @@
 import os
-import subprocess
-import time
 
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
-time.sleep(.25)
-pids = subprocess.check_output("fuser 9000/tcp").split()
-for pid in pids:
-    print("Killing " + pid)
-    os.popen('sudo kill ' + pid)
+os.popen("fuser -k 9000/tcp")
 exit()
