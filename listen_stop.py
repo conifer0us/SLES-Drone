3437c537a5b88217d31fa72d0aeb62931d17b3f0
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 16:53:22 -0500

    Prints pids to make sure process grabbing works

--- listen_stop.py
+++ listen_stop.py
@@ -3,6 +3,7 @@ import os
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
 pids = os.popen("sudo fuser 9000/tcp").read().replace("  "," ").split()
+print("".join(pids))
 for pid in pids:
     print("Killing " + pid)
     os.popen('sudo kill ' + pid)
