177d5666f0caeb5729bc474c136f950363698805
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 17:21:49 -0500

    changes back to os.system

--- listen_stop.py
+++ listen_stop.py
@@ -2,5 +2,5 @@ import os
 
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
-os.popen("fuser -k 9000/tcp")
+os.system("sudo fuser -k 9000/tcp")
 exit()
