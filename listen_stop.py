ba5e38f940407a3f13dd1af4f139ef43cb15efdd
BuildTools <unconfigured@null.spigotmc.org>
2021-04-28 16:45:35 -0500

    Changed pid to remove empty string from beginning

--- listen_stop.py
+++ listen_stop.py
@@ -3,8 +3,8 @@ import os
 input() # Stop until kill signal
 print("***Shutdown Signal Received. Program is shutting down***")
 pids = os.popen("sudo fuser 9000/tcp").read().replace("  "," ").split(" ")
-pids.remove("")
+pids.remove('')
 for pid in pids:
-    print("Killing "+pid)
+    print("Killing " + pid)
     os.popen('sudo kill ' + pid)
 exit()
